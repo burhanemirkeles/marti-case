d8178545d1de03fc7f00038e12b45310fc7103a9
Emir Keles <burhanemir.keles@halkbank.com.tr>
2025-05-08 17:17:47 +0300

    created new project

---
+++ MartiCase/MartiCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MartiCase
+//
+//  Created by Emir Keleş on 8.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
