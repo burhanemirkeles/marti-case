94ca5a03faca3972d7909700a85969b56f1d25a3
Emir Keles <burhanemir.keles@halkbank.com.tr>
2025-05-09 23:16:50 +0300

    add BottomBarView and MapView

--- MartiCase/MartiCase/ViewController.swift
+++ MartiCase/MartiCase/ViewController.swift
@@ -6,12 +6,46 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+  var bottomBar = BottomBar()
+
+  var mapView = MKMapView()
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.view.backgroundColor = .white
+    setupBottomBar()
+    setupMapView()
     // Do any additional setup after loading the view.
   }
-}
 
+  private func setupMapView() {
+    mapView.translatesAutoresizingMaskIntoConstraints = false
+    mapView.showsUserLocation = true
+    mapView.userTrackingMode = .follow
+    mapView.isZoomEnabled = true
+    view.addSubview(mapView)
+
+    NSLayoutConstraint.activate([
+      mapView.topAnchor.constraint(equalTo: view.topAnchor),
+      mapView.bottomAnchor.constraint(equalTo: bottomBar.topAnchor),
+      mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+    ])
+  }
+
+  private func setupBottomBar() {
+    view.addSubview(bottomBar)
+    bottomBar.translatesAutoresizingMaskIntoConstraints = false
+
+    NSLayoutConstraint.activate([
+      bottomBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      bottomBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      bottomBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+      bottomBar.heightAnchor.constraint(equalToConstant: 80)
+    ])
+  }
+}
